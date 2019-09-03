e0f732eb5f18d012e437cf9d590c1f6287c2d6e6
石昌琦 <shicq@knownsec.com>
2019-09-03 20:24:03 +0800

    强化 CI

--- .sh
+++ .sh
@@ -2,6 +2,7 @@ eval "lint-md _posts --config .lint-md.json"
 if [ $? -ne 0 ]; then
     echo "failed"
     eval "lint-md _posts --config .lint-md.json --fix"
+    echo "fixed"
 else
     echo "succeed"
 fi
